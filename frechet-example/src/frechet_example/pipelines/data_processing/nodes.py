8883bb240a110fd701cd420cfcb5217737f4388a
galenseilis <galen.seilis@seilis.ca>
2023-08-02 20:17:50 -0700

    Implemented node to load environmental chemistry data and mites

--- frechet-example/src/frechet_example/pipelines/data_processing/nodes.py
+++ frechet-example/src/frechet_example/pipelines/data_processing/nodes.py
@@ -2,3 +2,25 @@
 This is a boilerplate pipeline 'data_processing'
 generated using Kedro 0.18.12
 """
+
+import requests
+
+from kedro.pipeline import node
+import pandas as pd
+
+def load_env_chem_data():
+    '''Load the environmental chemistry data.'''
+    url = 'https://doi.org/10.1371/journal.pone.0282068.s001'
+    response = requests.get(url)
+    data = response.content
+    out_path = './data/01_raw/' +\
+            'journal.pone.0282068.s001.xlsx'
+
+    with open(out_path, 'wb') as f:
+        f.write(data)
+
+    return pd.read_excel(out_path)
+
+return_load_env_chem_data = node(func=load_env_chem_data, inputs=None, outputs="env_chem_raw_data")
+
+
--- frechet-example/src/frechet_example/pipelines/data_processing/pipeline.py
+++ frechet-example/src/frechet_example/pipelines/data_processing/pipeline.py
@@ -4,7 +4,7 @@ generated using Kedro 0.18.12
 """
 
 from kedro.pipeline import Pipeline, pipeline
-
+from . import nodes
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    return pipeline([nodes.return_load_env_chem_data])
